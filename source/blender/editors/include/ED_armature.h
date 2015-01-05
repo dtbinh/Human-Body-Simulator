f990745bc33cb15994050a7e5668b0ec4f605a73
Your Name <you@example.com>
2015-01-05 20:47:05 +1000

    Stupid error

--- source/blender/editors/include/ED_armature.h
+++ source/blender/editors/include/ED_armature.h
@@ -230,7 +230,7 @@ void ED_armature_validate_active(struct bArmature *arm);
 void ED_muscle_sync_selection(struct ListBase *edmu);
 
 EditArmatureElement *ED_armature_edit_bone_add_primitive(struct Object *obedit_arm, float length, bool view_aligned);
-EditArmatureElement *ED_armature_edit_bone_add(struct bArmature *arm, const char *name, int type);
+EditArmatureElement *ED_armature_edit_armature_element_add(struct bArmature *arm, const char *name, int type);
 void ED_armature_edit_bone_remove(struct bArmature *arm, EditBone *exBone);
 void ED_armature_edit_muscle_remove(struct bArmature *arm, EditMuscle *exMuscle);
 
