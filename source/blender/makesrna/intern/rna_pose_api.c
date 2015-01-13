650342349ee97cced8671506b475ba539efdf22a
Evangel63 <you@example.com>
2015-01-13 21:27:35 +1000

    data

--- source/blender/makesrna/intern/rna_pose_api.c
+++ source/blender/makesrna/intern/rna_pose_api.c
@@ -50,14 +50,14 @@
 #include "DNA_action_types.h" /* bPose */
 #include "BKE_armature.h"
 
-static float rna_PoseBone_do_envelope(bPoseChannel *chan, float *vec)
+static float rna_PoseElement_do_envelope(bPoseChannel *chan, float *vec)
 {
 	ArmatureElement *element = chan->bone;
 
-	float scale = (element->flag & BONE_MULT_VG_ENV) == BONE_MULT_VG_ENV ? ((BoneData*)element->custom)->weight : 1.0f;
+	float scale = (element->flag & ELEMENT_MULT_VG_ENV) == ELEMENT_MULT_VG_ENV ? ((BoneData*)element->data)->weight : 1.0f;
 
 	return distfactor_to_bone(vec, chan->pose_head, chan->pose_tail, element->rad_head * scale,
-	                          element->rad_tail * scale, ((BoneData*)element->custom)->dist * scale);
+	                          element->rad_tail * scale, ((BoneData*)element->data)->dist * scale);
 }
 #else
 
@@ -72,7 +72,7 @@ void RNA_api_pose_channel(StructRNA *srna)
 	PropertyRNA *parm;
 	FunctionRNA *func;
 
-	func = RNA_def_function(srna, "evaluate_envelope", "rna_PoseBone_do_envelope");
+	func = RNA_def_function(srna, "evaluate_envelope", "rna_PoseElement_do_envelope");
 	RNA_def_function_ui_description(func, "Calculate bone envelope at given point");
 	parm = RNA_def_float_vector_xyz(func, "point", 3, NULL, -FLT_MAX, FLT_MAX, "Point",
 	                                "Position in 3d space to evaluate", -FLT_MAX, FLT_MAX);
